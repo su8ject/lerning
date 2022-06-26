15245cc4627138050ed2c48c14837d1db863f975
su8ject <krivizyuk.andrei@gmail.com>
2022-06-26 12:33:15 +0300

    initial

---
+++ 1/index.js
@@ -0,0 +1,13 @@
+// start()
+
+// window.addEventListener('resize', start);
+
+// function start(){
+//     if (document.documentElement.clientWidth > 483){
+//         document.getElementsByClassName('hui')[0].style.color = 'red';
+//     } else {
+//         document.getElementsByClassName('hui')[0].style.color = 'green';
+//     }
+//   }
+
+
