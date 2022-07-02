55cb9745080c605db006319561eed5d408ea0b92
su8ject <krivizyuk.andrei@gmail.com>
2022-07-03 02:54:44 +0300

    first layout

--- 2/6/2/index.js
+++ 2/6/2/index.js
@@ -1,17 +1,17 @@
-const themeButton = document.querySelector('#theme')
-const root = document.querySelector(':root')
-const v = '--primary-color'
+const themeButton = document.querySelector("#theme");
+const root = document.querySelector(":root");
+const v = "--primary-color";
 
 const themeMap = {
-    'blue':'red',
-    'red':'black',
-    'black':'blue',
+  blue: "red",
+  red: "black",
+  black: "blue",
+};
+
+function handler(event) {
+  const color = getComputedStyle(root).getPropertyValue(v);
+
+  root.style.setProperty(v, themeMap[color]);
 }
 
-function handler(event){ 
-    const color = getComputedStyle(root).getPropertyValue(v)
-
-    root.style.setProperty(v, themeMap[color])
-}
-
-themeButton.addEventListener('click', handler)
+themeButton.addEventListener("click", handler);
