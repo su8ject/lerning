26ec9d75355b1c328de9a8e1bc361f08bbcaf295
su8ject <krivizyuk.andrei@gmail.com>
2022-08-16 11:11:46 +0300

    Update weather.js

--- 4/js/weather.js
+++ 4/js/weather.js
@@ -1,5 +1,5 @@
 let request = fetch(
-  "http://api.openweathermap.org/data/2.5/weather?q=Vysoke,ua&appid=0ee482c5e03972f5357e59bee44e3b64"
+  "https://api.openweathermap.org/data/2.5/weather?q=Vysoke,ua&appid=0ee482c5e03972f5357e59bee44e3b64"
 )
   .then((response) => response.json())
   .then((result) => console.log(result));
