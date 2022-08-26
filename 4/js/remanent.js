f6cf636b83685a8cf0babd8c78f5d2ba25f286d0
su8ject <krivizyuk.andrei@gmail.com>
2022-08-26 22:01:19 +0300

    added loader + fix

--- 4/js/remanent.js
+++ 4/js/remanent.js
@@ -1,34 +1,21 @@
-const baseUrl = "https://queenlyrain.backendless.app/api/data";
-
-const getIsFavourite = (key) =>
-  JSON.parse(localStorage.getItem("favouriteName").includes(key));
-
-const createCard = (item) => {
-  const isFavourite = getIsFavourite(item.name);
-  const isAvailable = item.availability;
-
-  return `<div class="products-card ${isAvailable ? "" : "sold"}">
-        <svg class="favourite-mark ${isFavourite ? "temp block" : ""}">
-          <use xlink:href="./img/sprite.svg#favourite-mark"></use>
-        </svg>
-      <div><img src="./img/products/${item.name}.jpg" class="card--img" /></div>
-        <div class="card-name">
-          <span class="card-name--text">${item.name}</span>
-          <span class="card-name--price">${item.price}грн</span>
-        </div>
-      </div>`;
-};
+const nameItems = {};
 
 try {
   fetch(`${baseUrl}/tools`)
     .then((response) => response.json())
     .then((result) => {
-      const gallery = document.querySelector(".galery"); // getElementById
-
       const gallaryCards = result.map((item) => createCard(item));
 
+      result.map((item) => (nameItems[item.name] = item.description));
+
       gallery.innerHTML = gallaryCards.join("\n");
+    })
+    .then(() => {
+      const mask = document.querySelector(".mask");
+      const html = document.querySelector("html");
+      mask.remove();
+      html.classList.add("active");
     });
-} catch (e) {
-  console.log(`Error: ${e.code} --- ${e.message}`);
+} catch {
+  gallery.innerHTML = createError();
 }
