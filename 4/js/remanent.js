186ed8548e144d8b4fda4e16278e9b1dfa102e38
su8ject <krivizyuk.andrei@gmail.com>
2022-08-26 22:08:36 +0300

    fix loader

--- 4/js/remanent.js
+++ 4/js/remanent.js
@@ -12,9 +12,7 @@ try {
     })
     .then(() => {
       const mask = document.querySelector(".mask");
-      const html = document.querySelector("html");
       mask.remove();
-      html.classList.add("active");
     });
 } catch {
   gallery.innerHTML = createError();
